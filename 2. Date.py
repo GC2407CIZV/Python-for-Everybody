875514d94a6865296054fb05d20d5391d4675ac5
Gregory Charles <gregory.charles01@gmail.com>
2025-01-10 21:44:02 +0900

    Delete 2. Date.py

--- 2. Date.py
+++
@@ -1,9 +0,0 @@
-# Import the datetime module
-import datetime
-
-# Get the current date
-current_date = datetime.datetime.now()
-
-# Display the current date
-print(f"Today's date is: {current_date.date()}")
-print(f"The current time is: {current_date.time()}")
